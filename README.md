61699670d82dbafa2c354a9b99e04cefa7128c23
‘Timothy <Timothy.viccari@gmail.com>
2021-06-23 20:13:54 -0400

    lab complete

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## This is my first Django app
 
+Lab Submission 1
 https://github.com/everydaytimmy/django-snacks/pull/2
+
+Lab Submission 2
+https://github.com/everydaytimmy/django-snacks/pull/3
--- snacks/urls.py
+++ snacks/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import AboutPageView, HomePageView, SnackListView, SnackDetailView
+from .views import (
+  AboutPageView,
+  HomePageView,
+  SnackCreateView,
+  SnackListView,
+  SnackDetailView,
+  SnackDeleteView,
+  SnackUpdateView
+)
 
 urlpatterns = [
   path('home/', HomePageView.as_view(), name='home'),
   path('about/', AboutPageView.as_view(), name='about'),
   path("snacks", SnackListView.as_view(), name='snack_list'),
-  path("snacks/<int:pk>/", SnackDetailView.as_view(), name='snack_detail')
-  
+  path("snacks/<int:pk>/", SnackDetailView.as_view(), name='snack_detail'),
+  path('create/', SnackCreateView.as_view(), name = 'snack_create'),
+  path('snacks/<int:pk>/update/', SnackUpdateView.as_view(), name = 'snack_update'),
+  path('snacks/<int:pk>/delete/', SnackDeleteView.as_view(), name = 'snack_delete'),
   ]
